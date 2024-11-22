179501608d4fd29d8c86bdae9afca2c6058be462
Ali <aliifarajj@hotmail.com>
2024-11-23 02:27:44 +0300

    commit

--- src/app/form/page.jsx
+++ src/app/form/page.jsx
@@ -2,30 +2,31 @@ import NameForm from "@/components/NameForm";
 import DeleteForm from "@/components/DeleteForm";
 import LoadingSpinner from "@/components/LoadingSpinner";
 
-const from = async () => {
-    const data = await fetch(`${process.env.DOMAIN_NAME}/api/name`, {cache: "force-cache"})
-    const names = await data.json()
-    return ( 
-        <div className="w-full h-full">
-            <h1>what is your name?</h1>
-           
-            <NameForm/>
+const formPage = async () => {
+  const data = await fetch(`${process.env.DOMAIN_NAME}/api/name`, {
+    cache: "force-cache",
+  });
+  const names = await data.json();
+  return (
+    <div className="w-full h-full">
+      <h1>what is your name?</h1>
 
-            <div>
-                {names ? 
-                
-                names?.map((name)=>(
-                    <div  key={name._id} className="flex gap-3 my-5">
-                        <h3>{name.name}</h3>
-                        <DeleteForm nameId={name._id}/>
-                    </div>
-                ))
-                :
-                <LoadingSpinner/>
-            }
+      <NameForm />
+
+      <div>
+        {names ? (
+          names?.map((name) => (
+            <div key={name._id} className="flex gap-3 my-5">
+              <h3>{name.name}</h3>
+              <DeleteForm nameId={name._id} />
             </div>
-        </div>
-     );
-}
- 
-export default from;
+          ))
+        ) : (
+          <LoadingSpinner />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default formPage;
