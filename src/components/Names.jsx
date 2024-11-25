a45b3a83562b34950a707708f6e3398f6a9d86b7
Ali <aliifarajj@hotmail.com>
2024-11-26 02:27:08 +0300

    commit14

--- src/components/Names.jsx
+++ src/components/Names.jsx
@@ -2,18 +2,17 @@ import DeleteForm from "@/components/DeleteForm";
 import Name from "@/lib/models/Name";
 
 const Names = async () => {
-  // const data = await fetch("https://power-g.vercel.app/api/name", {cache: "force-cache"})
-  // const names = await data.json()
+  const data = await fetch(`${process.env.DOMAIN_NAME}/api/name`, {cache: "force-cache"})
+  const names = await data.json()
  
   return (
     <div>
-      {/* {names?.map((name) => (
+      {names?.map((name) => (
         <div key={name._id} className="flex gap-3 my-5">
           <h3>{name.name}</h3>
          
         </div>
-      ))} */}
-      k
+      ))}
     </div>
   );
 };
